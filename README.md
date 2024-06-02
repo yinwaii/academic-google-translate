47786d46d3e7f7d2af05841540525259b4093a1c
歪 <yinwai@yinwai.local>
2024-06-02 10:36:45 +0800

    v1.1 支持ieeexplore

--- README.md
+++ README.md
@@ -1,20 +1,15 @@
 # 谷歌翻译一键增强-智能去除换行、论文翻译优化
+
 本插件用于增强谷歌翻译学术论文的使用体验，助力科研。新特性持续实现中，如果功能需求欢迎提交[Issue](https://github.com/yinwaii/academic-google-translate/issues)或[反馈](https://greasyfork.org/zh-CN/scripts/494878-%E8%B0%B7%E6%AD%8C%E7%BF%BB%E8%AF%91%E8%87%AA%E5%8A%A8%E5%8E%BB%E9%99%A4%E6%8D%A2%E8%A1%8C/feedback)。
 
 使用方式：安装即生效
 
 主要功能：
 1. 智能检测pdf复制文本时产生的换行符，同时保留段落形状(避免变成一段)。
-2. 在翻译arxiv网页版论文(ar5iv)时，避免排版混乱，避免翻译作者、图表、引用、参考文献等。
-## 智能去除换行
-在复制pdf文本时，经常会出现换行符，导致翻译质量下降、可读性降低，本插件智能检测pdf复制文本时产生的换行符，同时保留段落形状(避免变成一段)，最终可达到近似原文档的效果。
-
-### 优化效果
-优化前：<img src="https://github.com/yinwaii/academic-google-translate/blob/main/imgs/translate-raw.png?raw=true" alt="无优化" width="40%" />
-优化后：<img src="https://github.com/yinwaii/academic-google-translate/blob/main/imgs/translate-new.png?raw=true" alt="优化后" width="40%" />
+2. 在翻译arxiv和ieeexplore网页版论文时，避免排版混乱，避免翻译作者、图表、引用、参考文献等。
 
 ## 论文一键翻译优化
-在翻译arxiv网页版论文(ar5iv)时，本插件可以避免排版混乱，避免翻译作者、图表、引用、参考文献等。
+在翻译arxiv和ieeexplore网页版论文时，本插件可以避免排版混乱，避免翻译作者、图表、引用、参考文献等。
 
 ### 如何使用arxiv浏览网页版论文
 
@@ -28,11 +23,27 @@
 2. 将地址栏的`arxiv`改为`ar5iv`，如 https://ar5iv.org/abs/2106.00001 ，访问该网页
 3. 使用谷歌翻译插件翻译该网页
 
-### 优化效果
+#### 优化效果
 优化前：<img src="https://github.com/yinwaii/academic-google-translate/blob/main/imgs/arxiv-raw.png?raw=true" alt="无优化" width="40%" />
 优化后：<img src="https://github.com/yinwaii/academic-google-translate/blob/main/imgs/arxiv-new.png?raw=true" alt="优化后" width="40%" />
 
+### 如何使用ieeexplore浏览网页版论文
+1. 使用ieeexplore访问所需论文，如 https://ieeexplore.ieee.org/document/9355319
+2. 使用谷歌翻译插件翻译该网页
+
+#### 优化效果
+优化前：<img src="https://github.com/yinwaii/academic-google-translate/blob/main/imgs/ieeexplore-raw.png?raw=true" alt="无优化" width="40%" />
+优化后：<img src="https://github.com/yinwaii/academic-google-translate/blob/main/imgs/ieeexplore-new.png?raw=true" alt="优化后" width="40%" />
+
+## 智能去除换行
+在复制pdf文本时，经常会出现换行符，导致翻译质量下降、可读性降低，本插件智能检测pdf复制文本时产生的换行符，同时保留段落形状(避免变成一段)，最终可达到近似原文档的效果。
+
+### 优化效果
+优化前：<img src="https://github.com/yinwaii/academic-google-translate/blob/main/imgs/translate-raw.png?raw=true" alt="无优化" width="40%" />
+优化后：<img src="https://github.com/yinwaii/academic-google-translate/blob/main/imgs/translate-new.png?raw=true" alt="优化后" width="40%" />
+
 ## 更新日志
+- 2024-06-02: 发布v1.1版本，支持对ieeexplore网页版论文的翻译优化
 - 2024-05-31: 发布v1.0版本，实现arxiv论文翻译优化，编写说明文档，提升易用性
 - 2024-05-18: 发布v0.2版本，实现智能去除换行
 
--- src.js
+++ src.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         谷歌翻译一键增强-智能去除换行、论文翻译优化
-// @version      1.0.1
+// @version      1.1
 // @license      MIT
-// @description  ① 智能检测pdf复制文本时产生的换行符，同时保留段落形状(避免变成一段)。② 在翻译arxiv网页版论文(ar5iv)时，避免排版混乱，避免翻译作者、图表、引用、参考文献等。
+// @description  ① 智能检测pdf复制文本时产生的换行符，同时保留段落形状(避免变成一段)。② 在翻译arxiv和ieeexplore网页版论文时，避免排版混乱，避免翻译作者、图表、引用、参考文献等。
 // @author       yinwaii
 // @homeurl      https://github.com/yinwaii/academic-google-translate/
 // @homeurl      https://greasyfork.org/zh-CN/scripts/494878
@@ -11,12 +11,13 @@
 // @match        *://ar5iv.labs.arxiv.org/*
 // @match        *://*.ar5iv.*/*
 // @match        *://arxiv.org/*
+// @match        *://ieeexplore.ieee.org/*
 // @match        *://*.translate.goog/*
 // @connect      translate.google.com
 // @grant        unsafeWindow
 // @namespace    http://tampermonkey.net/
-// @downloadURL  https://update.greasyfork.org/scripts/494878/%E8%B0%B7%E6%AD%8C%E7%BF%BB%E8%AF%91%E8%87%AA%E5%8A%A8%E5%8E%BB%E9%99%A4%E6%8D%A2%E8%A1%8C.user.js
-// @updateURL    https://update.greasyfork.org/scripts/494878/%E8%B0%B7%E6%AD%8C%E7%BF%BB%E8%AF%91%E8%87%AA%E5%8A%A8%E5%8E%BB%E9%99%A4%E6%8D%A2%E8%A1%8C.meta.js
+// @downloadURL  https://update.greasyfork.org/scripts/494878/%E8%B0%B7%E6%AD%8C%E7%BF%BB%E8%AF%91%E4%B8%80%E9%94%AE%E5%A2%9E%E5%BC%BA-%E6%99%BA%E8%83%BD%E5%8E%BB%E9%99%A4%E6%8D%A2%E8%A1%8C%E3%80%81%E8%AE%BA%E6%96%87%E7%BF%BB%E8%AF%91%E4%BC%98%E5%8C%96.user.js
+// @updateURL    https://update.greasyfork.org/scripts/494878/%E8%B0%B7%E6%AD%8C%E7%BF%BB%E8%AF%91%E4%B8%80%E9%94%AE%E5%A2%9E%E5%BC%BA-%E6%99%BA%E8%83%BD%E5%8E%BB%E9%99%A4%E6%8D%A2%E8%A1%8C%E3%80%81%E8%AE%BA%E6%96%87%E7%BF%BB%E8%AF%91%E4%BC%98%E5%8C%96.meta.js
 // ==/UserScript==
 
 (function () {
@@ -42,6 +43,7 @@
 	function preprocess_ar5iv() {
 		const ar5ivSelector = [
 			'pre',
+			'head',
 			'.ltx_Math, .ltx_equationgroup, .ltx_equation',
 			'.ltx_cite',
 			'.ltx_ref',
@@ -51,20 +53,21 @@
 			'.ltx_note_mark',
 			'.ltx_authors',
 			'.ltx_tabular',
+			'.ltx_listing :not(figcaption)',
+			'.ltx_lstlisting :not(figcaption)',
 			'.ltx_figure :not(figcaption)',
 			'.ltx_picture :not(figcaption)',
 			'.ltx_table :not(figcaption)',
 			'.ltx_flex_cell :not(figcaption)',
 			'semantics',
 			'.ltx_algorithm',
-			'.ltx_text.ltx_font_bold',
+			//'.ltx_text.ltx_font_bold',
 			//'.ltx_text.ltx_font_italic',
-			//'.ltx_flex_figure, .ltx_graphics, .ltx_img_square',
 		];
 		function addNoTranslateAttr(array) {
 			array.forEach((name) => {
 				[...document.querySelectorAll(name)].forEach((node) => {
-					if (node.className.indexOf("notranslate") === -1) {
+					if (node?.className?.indexOf && node.className.indexOf("notranslate") === -1) {
 						node.classList.add("notranslate");
 					}
 				});
@@ -72,6 +75,30 @@
 			console.log(document.querySelectorAll('.notranslate'));
 		}
 		addNoTranslateAttr(ar5ivSelector);
+	};
+	function preprocess_ieee() {
+		const ieeeSelector = [
+			'xpl-document-header',
+			'xpl-related-article-list',
+			'.document-tabs',
+			'head',
+			'title',
+			'meta',
+			'tex-math',
+			'h1,h2,h3,h4,h5',
+			'i', 'a',
+		];
+		function addNoTranslateAttr(array) {
+			array.forEach((name) => {
+				[...document.querySelectorAll(name)].forEach((node) => {
+					if (node?.className?.indexOf && node.className.indexOf("notranslate") === -1) {
+						node.classList.add("notranslate");
+					}
+				});
+			});
+			console.log(document.querySelectorAll('.notranslate'));
+		}
+		addNoTranslateAttr(ieeeSelector);
 	}
 	function detect_flush() {
 		window.addEventListener('message', e => {
@@ -104,10 +131,24 @@
 	if (window.location.hostname.indexOf("arxiv.org") !== -1) {
 		window.onload = () => {
 			let el = document.querySelector('.title');
-			console.log(el);
+			//console.log(el);
 			el.innerHTML += ` &nbsp;<a href="https://translate.google.com/translate?sl=auto&tl=zh-CN&u=https://ar5iv.org${window.location.pathname}">查看翻译</a>`;
-			console.log(el);
+			//console.log(el);
 		}
+	}
+
+	if (window.location.hostname.indexOf("ieeexplore") !== -1) {
+		window.onload = () => {
+			let el = document.querySelector('.document-title');
+			//console.log(el);
+			//el.innerHTML += ` &nbsp;<a href="https://translate.google.com/translate?sl=auto&tl=zh-CN&u=${window.location.hostname}${window.location.pathname}">查看翻译</a>`;
+			//console.log(el);
+			setTimeout(function () {
+				preprocess_ieee();
+				if (window.location.hostname.indexOf("translate.goog") !== -1)
+					window.frames[0].postMessage('flush', '*');
+			}, 100);
+		};
 	}
 
 })();
